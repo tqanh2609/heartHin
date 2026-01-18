2be596560f4024aef13072930325b3a73501fa1d
Quang Anh <quanganhtran4000@gmail.com>
2026-01-18 22:17:39 +0700

    edit vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Set base to your repository name: /repo-name/
-  base: '/heart_web/',
+  base: '/heartHin/',
 });
